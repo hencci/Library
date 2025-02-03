f29f54165e1263e110079e2cde56c37983978fee
hencci <hencci20@gmail.com>
2025-02-03 08:45:05 +0100

    Make some DOM selection and save in variables

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function Book(title, author, pages, read) {
     };
 }
 
+let bookContainer = document.querySelector(".books");
+let newBookBoard = document.querySelector("#new-entry");
+let newBookSubmitButton = document.querySelector("#new-book-submit");
+let newBookButton = document.querySelector(".add-new");
+
 function addBookToLibrary(title, author, pages, read) {
-    
 }
